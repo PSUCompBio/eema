436a42036167ed76e021fe41230f4186e07e4a8f
JESSE IAN GERBER <jig6@comp-int-04.aci.ics.psu.edu>
2017-04-19 16:06:48 -0400

    removed obsolete lines

--- src/FEM/SolidMechanics/fe_calculateFR.cpp
+++ src/FEM/SolidMechanics/fe_calculateFR.cpp
@@ -9,13 +9,10 @@ VectorXd fe_calculateFR(int sdof, VectorXd fi_curr, VectorXd mm, VectorXd A){
  for(int i=0;i<bc_types;i++){
 		std::string type = bc[i].getType();
 		if(type=="displacement"){
-			//double input_disp_amp = bc[i].getAmplitude();
-			//std::string disp_curve = bc[i].getTimeBehavior();
 			int number_of_dof = bc[i].getNumDOF();
 			VectorXi local_dof = bc[i].getDOF();
 			for(int j=0;j<number_of_dof;j++){
 				int c = local_dof(j);
-				//U(c) = fe_function(input_disp_amp,disp_curve,time);
 				fr(c) = fi_curr(c) + mm(c) * A(c);
 			}
 		}
